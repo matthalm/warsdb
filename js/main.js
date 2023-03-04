d33f8013f1c4faa748f9d62cad41ce876f0ee712
matthalm <33130412+matthalm@users.noreply.github.com>
2023-03-03 22:06:30 -0500

    tag filters

--- js/main.js
+++ js/main.js
@@ -25,6 +25,108 @@ function select_all_sets() {
     }
 }
 
+function subselect() {
+    document.getElementById("weapon-tags").selectedIndex = 0;
+    document.getElementById("asset-tags").selectedIndex = 0;
+    document.getElementById("earther-tags").selectedIndex = 0;
+    document.getElementById("gongen-tags").selectedIndex = 0;
+    document.getElementById("maverick-tags").selectedIndex = 0;
+    document.getElementById("shi-tags").selectedIndex = 0;
+    document.getElementById("quay-tags").selectedIndex = 0;
+    document.getElementById("independent-tags").selectedIndex = 0;
+    if (document.getElementById("select-type").value == ".type-weapon") {
+        document.getElementById("asset-tags").style.display = "none";
+        document.getElementById("weapon-tags").style.display = "inline-block";
+        document.getElementById("earther-tags").style.display = "none";
+        document.getElementById("gongen-tags").style.display = "none";
+        document.getElementById("maverick-tags").style.display = "none";
+        document.getElementById("shi-tags").style.display = "none";
+        document.getElementById("quay-tags").style.display = "none";
+        document.getElementById("independent-tags").style.display = "none";
+    } else if (document.getElementById("select-type").value == ".type-asset") {
+        document.getElementById("asset-tags").style.display = "inline-block";
+        document.getElementById("weapon-tags").style.display = "none";
+        document.getElementById("earther-tags").style.display = "none";
+        document.getElementById("gongen-tags").style.display = "none";
+        document.getElementById("maverick-tags").style.display = "none";
+        document.getElementById("shi-tags").style.display = "none";
+        document.getElementById("quay-tags").style.display = "none";
+        document.getElementById("independent-tags").style.display = "none";
+    } else {
+    switch (document.getElementById("select-faction").value) {
+    case ".faction-earther":
+        document.getElementById("weapon-tags").style.display = "none";
+        document.getElementById("asset-tags").style.display = "none";
+        document.getElementById("earther-tags").style.display = "inline-block";
+        document.getElementById("gongen-tags").style.display = "none";
+        document.getElementById("maverick-tags").style.display = "none";
+        document.getElementById("shi-tags").style.display = "none";
+        document.getElementById("quay-tags").style.display = "none";
+        document.getElementById("independent-tags").style.display = "none";
+        break;
+    case ".faction-gongen":
+        document.getElementById("weapon-tags").style.display = "none";
+        document.getElementById("asset-tags").style.display = "none";
+        document.getElementById("earther-tags").style.display = "none";
+        document.getElementById("gongen-tags").style.display = "inline-block";
+        document.getElementById("maverick-tags").style.display = "none";
+        document.getElementById("shi-tags").style.display = "none";
+        document.getElementById("quay-tags").style.display = "none";
+        document.getElementById("independent-tags").style.display = "none";
+        break;
+    case ".faction-maverick":
+        document.getElementById("weapon-tags").style.display = "none";
+        document.getElementById("asset-tags").style.display = "none";
+        document.getElementById("earther-tags").style.display = "none";
+        document.getElementById("gongen-tags").style.display = "none";
+        document.getElementById("maverick-tags").style.display = "inline-block";
+        document.getElementById("shi-tags").style.display = "none";
+        document.getElementById("quay-tags").style.display = "none";
+        document.getElementById("independent-tags").style.display = "none";
+       break;
+    case ".faction-shi":
+        document.getElementById("weapon-tags").style.display = "none";
+        document.getElementById("asset-tags").style.display = "none";
+        document.getElementById("earther-tags").style.display = "none";
+        document.getElementById("gongen-tags").style.display = "none";
+        document.getElementById("maverick-tags").style.display = "none";
+        document.getElementById("shi-tags").style.display = "inline-block";
+        document.getElementById("quay-tags").style.display = "none";
+        document.getElementById("independent-tags").style.display = "none";
+        break; 
+    case ".faction-quay":
+        document.getElementById("weapon-tags").style.display = "none";
+        document.getElementById("asset-tags").style.display = "none";
+        document.getElementById("earther-tags").style.display = "none";
+        document.getElementById("gongen-tags").style.display = "none";
+        document.getElementById("maverick-tags").style.display = "none";
+        document.getElementById("shi-tags").style.display = "none";
+        document.getElementById("quay-tags").style.display = "inline-block";
+        document.getElementById("independent-tags").style.display = "none";
+        break
+    case ".faction-independent":
+        document.getElementById("weapon-tags").style.display = "none";
+        document.getElementById("asset-tags").style.display = "none";
+        document.getElementById("earther-tags").style.display = "none";
+        document.getElementById("gongen-tags").style.display = "none";
+        document.getElementById("maverick-tags").style.display = "none";
+        document.getElementById("shi-tags").style.display = "none";
+        document.getElementById("quay-tags").style.display = "none";
+        document.getElementById("independent-tags").style.display = "inline-block";
+        break
+    default:
+        document.getElementById("weapon-tags").style.display = "none";
+        document.getElementById("asset-tags").style.display = "none";
+        document.getElementById("earther-tags").style.display = "none";
+        document.getElementById("gongen-tags").style.display = "none";
+        document.getElementById("maverick-tags").style.display = "none";
+        document.getElementById("shi-tags").style.display = "none";
+        document.getElementById("quay-tags").style.display = "none";
+        document.getElementById("independent-tags").style.display = "none";
+    }
+    }
+}
+
 function filter() {
 
     resetCards();
@@ -39,14 +141,16 @@ function filter() {
 
     let selectedFaction = document.getElementById("select-faction").value;
     let selectedType = document.getElementById("select-type").value;
+    let selectedTags = document.getElementById("earther-tags").value + document.getElementById("gongen-tags").value + document.getElementById("maverick-tags").value + document.getElementById("shi-tags").value + document.getElementById("quay-tags").value + document.getElementById("independent-tags").value + document.getElementById("weapon-tags").value + document.getElementById("asset-tags").value;
+    let selections = selectedFaction + selectedType + selectedTags;
 
     var setList = [];
 
-    if (document.getElementById("toggle-Incursion").checked == true) {setList.push(".set-1"+selectedFaction+selectedType);}
-    if (document.getElementById("toggle-Nowhere").checked == true) {setList.push(".set-2"+selectedFaction+selectedType);}
-    if (document.getElementById("toggle-Edge").checked == true) {setList.push(".set-3"+selectedFaction+selectedType);}
-    if (document.getElementById("toggle-Motion").checked == true) {setList.push(".set-4"+selectedFaction+selectedType);}
-    if (document.getElementById("toggle-Eye").checked == true) {setList.push(".set-5"+selectedFaction+selectedType);}
+    if (document.getElementById("toggle-Incursion").checked == true) {setList.push(".set-1"+selections);}
+    if (document.getElementById("toggle-Nowhere").checked == true) {setList.push(".set-2"+selections);}
+    if (document.getElementById("toggle-Edge").checked == true) {setList.push(".set-3"+selections);}
+    if (document.getElementById("toggle-Motion").checked == true) {setList.push(".set-4"+selections);}
+    if (document.getElementById("toggle-Eye").checked == true) {setList.push(".set-5"+selections);}
 
     let checkedSets = setList.join(', ');
     let cards = document.querySelectorAll(checkedSets);
